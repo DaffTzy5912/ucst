75b3bee7a7adf4e63a68d6dd2ef2f3088ddd3f9e
DaffTzy5912 <dafanation999@gmail.com>
2025-04-27 20:54:10 +0700

    Update stalk.js

--- api/stalk.js
+++ api/stalk.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(`https://api.siputzx.my.id/api/stalk/tiktok?username=${username}`);
     const data = await response.json();
-    res.status(200).json(data);
+    res.status(200).json(data); // balikin response API apa adanya
   } catch (error) {
     res.status(500).json({ status: false, message: "Gagal mengambil data", error: error.message });
   }
