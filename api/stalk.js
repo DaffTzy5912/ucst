fef8d9c20fe14fdf91cd22f0f0d4659db55b3c22
DaffTzy5912 <dafanation999@gmail.com>
2025-04-27 20:48:53 +0700

    Create stalk.js

---
+++ api/stalk.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const { username } = req.query;
+
+  if (!username) {
+    return res.status(400).json({ status: false, message: "Username tidak ditemukan" });
+  }
+
+  try {
+    const response = await fetch(`https://api.siputzx.my.id/api/stalk/tiktok?username=${username}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Gagal mengambil data", error: error.message });
+  }
+}
